8c6bc3e22616803f4652d16ec0a122b56adbcc94
Elenilson <Elenilson Monteiro>
2021-03-02 20:01:13 -0300

    criação do repository e service do pedido

---
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/service/PedidoService.java
@@ -0,0 +1,75 @@
+package br.com.casamagalhaes.workshop.desafio.service;
+
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.com.casamagalhaes.workshop.desafio.enums.StatusPedido;
+import br.com.casamagalhaes.workshop.desafio.model.ItemPedido;
+import br.com.casamagalhaes.workshop.desafio.model.Pedido;
+import br.com.casamagalhaes.workshop.desafio.repository.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository repository;
+
+    public Page<Pedido> findAll(Integer pagina, Integer tamanho) {
+        Pageable pageable = PageRequest.of(pagina, tamanho);
+        return repository.findAll(pageable);
+    }
+
+    public Pedido findById(Long id) {
+        return repository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    public Pedido save(Pedido pedido) {
+        if (pedido.getItens().isEmpty()) {
+            throw new NullPointerException("pedido de venda sem produtos");
+        }
+
+        pedido.setStatus(StatusPedido.PENDENTE);
+        pedido.setValorTotalProdutos(valorTotalDosProdutos(pedido.getItens()));
+        pedido.setValorTotal(pedido.getValorTotalProdutos() + pedido.getTaxa());
+
+        return repository.saveAndFlush(pedido);
+    }
+
+    public Pedido update(Long id, Pedido pedido) {
+        if (repository.existsById(id)) {
+            Pedido pedidoAntigo = findById(id);
+
+            if (pedido.getStatus() != pedidoAntigo.getStatus()) {
+                throw new UnsupportedOperationException("STATUS do pedido informado diferente do atual.");
+            }
+
+            if (id != pedido.getId()) {
+                throw new UnsupportedOperationException("ID informado diferente do Pedido.");
+            }
+
+            return repository.saveAndFlush(pedido);
+        } else
+            throw new EntityNotFoundException("Pedido id: " + pedido.getId());
+
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+    private Double valorTotalDosProdutos(List<ItemPedido> itens) {
+        Double valor = 0.0;
+        for (ItemPedido itemPedido : itens) {
+            valor = valor + (itemPedido.getPrecoUnitario() * itemPedido.getQuantidade());
+        }
+        return valor;
+    }
+
+}
