6dcc382bec4a6573da23f36eefb3010a3e265923
Elenilson <Elenilson Monteiro>
2021-03-02 18:54:42 -0300

    criação dos modelos pedido e itemPedido

---
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/model/ItemPedido.java
@@ -0,0 +1,36 @@
+package br.com.casamagalhaes.workshop.desafio.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "item")
+    private Long id;
+
+    @NotEmpty(message = "descrição do item é obrigatória")
+    private String descricao;
+
+    private String precoUnitario;
+
+    private Long quantidade;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pedido")
+    private Pedido pedido;
+}
---
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/model/Pedido.java
@@ -0,0 +1,53 @@
+package br.com.casamagalhaes.workshop.desafio.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import br.com.casamagalhaes.workshop.desafio.enums.StatusPedido;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "pedido")
+    private Long id;
+
+    @NotEmpty(message = "nome do cliente é obrigatório")
+    private String nomeCliente;
+
+    @NotEmpty(message = "endereço do cliente é obrigatório")
+    private String endereco;
+
+    @NotEmpty(message = "telefone do cliente é obrigatório")
+    @Size(min = 7, max = 11, message = "número de telefone celular brasileiro deve ter no mínimo 7 e no máximo 11 caracteres")
+    private String telefone;
+
+    private Double valorTotalProdutos;
+
+    private Double taxa;
+
+    private Double valorTotal;
+
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedido> itens;
+
+}
